5bf2c5f9e69442f9addb0a1f3ba60eade239d00c
Evan <Donoyster@icloud.com>
2025-05-14 15:26:07 -0700

    nah
    hin

---
+++ CLickyCratesP8Perez/Assets/Scripts/Target.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTourque = 10;
+    private float xRange = 4;
+    private float yRangePos = -6;
+    private float ySpawnPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+
+        targetRb.AddForce (RandomForce(), ForceMode.Impulse);
+        targetRb.AddForce(RandomTourque(), RandomTourque(), RandomTourque(), ForceMode.Impulse);
+
+        transform.position = RandomSpawnPos();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTourque()
+    {
+        return Random.Range (-maxTourque,maxTourque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+}
