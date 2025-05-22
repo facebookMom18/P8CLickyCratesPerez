b5f412214f3e8a32ade32d7b4b003b70ffa61925
Evan <Donoyster@icloud.com>
2025-05-22 15:22:37 -0700

    daf
    fs

--- CLickyCratesP8Perez/Assets/Scripts/GameManager.cs
+++ CLickyCratesP8Perez/Assets/Scripts/GameManager.cs
@@ -2,18 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     private float spawnRate = 1.0f;
     private int score;
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    public Button restartButton;
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(SpawnTarget());
+        isGameActive = true;
         score = 0;
+        StartCoroutine(SpawnTarget());
         UpdateScore(0);
         
     }
@@ -25,12 +30,12 @@ public class GameManager : MonoBehaviour
     }
     IEnumerator SpawnTarget()
     {
-        while (true) {
-            yield return new  WaitForSeconds(spawnRate);
+        while (isGameActive) {
+            yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
             Instantiate(targets[index]);
 
-           
+
         }
     }
     public void UpdateScore(int scoreToAdd)
@@ -38,4 +43,15 @@ public class GameManager : MonoBehaviour
         score += scoreToAdd;
         scoreText.text = "Score:" + score;
     }
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive=false;
+    }
+    public void RestartGame()
+    {
+       
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name)
+;   }
 }
